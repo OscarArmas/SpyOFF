bb67c9424acc733db6df5a381a37d7fb352134cf
OscarArmas <saga_cth@hotmail.com>
2020-08-05 18:27:36 -0500

    arreglos para linux

--- offcase.py
+++ offcase.py
@@ -16,12 +16,12 @@ def update_full_sneaker_dic():
     #Extraemos solo informacion importante
     df = pd.concat(dataframes, sort=False)#[['id','title','portrait_url','url']]
 
-    #df = 
+    #df =
     #df.loc[df['id'] == 'eff479e3-dab9-38e3-b814-4e8b49f371d6']
 
     df =df.drop_duplicates(subset=['title'])
     df.to_csv('data/liquidacion/dic_products/productsLiq.csv', encoding='utf-8')
-    
+
 
 def check_new_list_sneaker(day = None):
     #leemos el archivo con todos los tenis que han estad disponibles en liquidacion
@@ -34,16 +34,16 @@ def check_new_list_sneaker(day = None):
         actual_day = today.strftime("%Y-%m-%d")
     else:
         actual_day = day
-        
+
     #leemos el archivo de el dia de hoy
     file_name= actual_day+'.json'
-    if os.path.isfile('data/liquidacion/'+file_name):
+    if os.path.isfile('data/Liquidacion/'+file_name):
         file_= ReportTM_LIQ('data/Liquidacion/')
         df_today = file_.generate_DF(file_name)
         s = set(entire_df.title.values)
         temp3 = [x for x in df_today.title.values if x not in s]
         df_today_util = df_today[df_today['title'].isin(temp3)]
-        if df_today_util.shape[0] <0 : 
+        if df_today_util.shape[0] <0 :
             return False
         df_today_util= df_today_util.drop(['id','colorDescription','Stock','isNew','subtitle','date','squarishURL' ],1)
         print(df_today_util.columns)
@@ -51,13 +51,13 @@ def check_new_list_sneaker(day = None):
         df_today_util['url'] = df_today_util['url'].apply(lambda x: 'https://www.nike.com/mx'+x[13:])
         df_today_util['url'] = df_today_util['url'].apply(lambda x: unidecode.unidecode(x))
 
-        
+
         '''
-        
-        df_all_new_products = df_today.merge(entire_df.drop_duplicates(), on=['title'], 
+
+        df_all_new_products = df_today.merge(entire_df.drop_duplicates(), on=['title'],
                        how='left', indicator=True)
         df_all_new_products = df_all_new_products[df_all_new_products['_merge'] == 'left_only']
-        if df_all_new_products.shape[0] <0 : 
+        if df_all_new_products.shape[0] <0 :
             return False
         print(df_all_new_products.columns.values)
         df_all_new_products = df_all_new_products.drop(['id_x','colorDescription','Stock','isNew','subtitle','date','id_y','portrait_url_y','url_y','_merge','squarishURL'], 1)
@@ -74,11 +74,9 @@ def check_new_list_sneaker(day = None):
 
 
 
-data = check_new_list_sneaker('2020-07-30')
+data = check_new_list_sneaker()
 print(data)
 if data:
 	send_email_Bcase1(data, ['saga_cth@hotmail.com'])
 else:
 	print('No hubo cambios')
-
-
