42606bedad6a74aa19cb9a2c97b78b3fc127aa83
OscarArmas <saga_cth@hotmail.com>
2021-04-01 12:10:42 -0600

    fix tiny bug

--- utils/NikeCrawl.py
+++ utils/NikeCrawl.py
@@ -14,8 +14,8 @@ class nike_crawler:
     date_today = ''
     state_pages=[]
     base_url = 'https://api.nike.com/cic/browse/v1?queryid=products&anonymousId=199C86923837F9429AB85910D6EC7B97&country=mx&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(MX)%26filter%3Dlanguage(es-419)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c)%26anchor%3D24%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=es-419&localizedRangeStr=%7BlowestPrice%7D%20%E2%80%94%20%7BhighestPrice%7D'
-    
-    
+
+
     def obtain_pre_info(self):
         self.date_today = str(date.today())
 
@@ -27,25 +27,25 @@ class nike_crawler:
 
             #vemos todas las paginas disponibles
             self.total_pages = base_json.get('data')['products']['pages']['totalPages']
-            
+
             last_product = self.total_pages * 24
             self.array_anchors = list(range(0, last_product,24))
-    
+
         except:
             print('No se pudo acceder al url')
             self.errors.append(1)
-            
-            
+
+
     def obtain_save_data(self):
         #llamamos la funcion de llenado de informacio basica para la clase
         self.obtain_pre_info()
-        
+
         assert len(self.errors) == 0
-        assert len(self.array_anchors) > 5
+        #assert len(self.array_anchors) > 5
         for anchor in self.array_anchors:
             try:
                 base_page = 'https://api.nike.com/cic/browse/v1?queryid=products&anonymousId=199C86923837F9429AB85910D6EC7B97&country=mx&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(MX)%26filter%3Dlanguage(es-419)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c)%26anchor%3D{}%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=es-419&localizedRangeStr=%7BlowestPrice%7D%20%E2%80%94%20%7BhighestPrice%7D'.format(anchor)
-                    
+
                 base_request = requests.get(base_page)
                 self.full_product_list = self.full_product_list + base_request.json()['data']['products']['products']
                 self.state_pages.append('OK')
@@ -53,20 +53,20 @@ class nike_crawler:
                 print('No se pudo acceder al url')
                 self.errors.append(1)
                 self.state_pages.append('ERROR')
-            
+
         assert len(self.errors) == 0
         assert self.date_today != ''
-            
+
         #guardamos todos los datos
         with open('data/%s.json'%self.date_today, 'w', encoding='utf-8') as f:
             json.dump(self.full_product_list, f, ensure_ascii=False, indent=4)
-            
+
         #Imprimimos el verbose
         print(self.verbose())
 
 
-            
-        
+
+
     def verbose(self):
         text= 'STATUS: ' + self.date_today+'\n'
         #print('STATUS:',self.date_today )
@@ -118,7 +118,7 @@ class ReportTM:
             Data_products['subtitle'].append(product['subtitle'])
             Data_products['title'].append(product['title'])
             Data_products['date'].append(name_file[:-5])
-        
+
         return pd.DataFrame(Data_products)
     #Funcion para generar la serie de tiempo, de los archivos JSON indicados, en ORDEN
     def generate_time_serie(self,*files):
@@ -148,7 +148,7 @@ class ReportTM:
     #NOTA:
     #Solo se ingresa la fecha del dia despues, el dia anterior lo se obtiene dentro de la funcion
     def check_changes_2days(self, day):
-    
+
         #obtenemos el dia anterior a la fecha dada
         date = datetime.strptime(day, "%Y-%m-%d")
         modified_date = date - timedelta(days=1)
@@ -182,8 +182,8 @@ class nike_crawler_Liquidacion:
     date_today = ''
     state_pages=[]
     base_url = 'https://api.nike.com/cic/browse/v1?queryid=products&anonymousId=DE9CEB2891D6D710B49C8CABEDC222DC&country=mx&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(MX)%26filter%3Dlanguage(es-419)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c%2C5b21a62a-0503-400c-8336-3ccfbff2a684)%26anchor%3D0%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=es-419&localizedRangeStr=%7BlowestPrice%7D%20%E2%80%94%20%7BhighestPrice%7D'
-    
-    
+
+
     def obtain_pre_info(self):
         self.date_today = str(date.today())
 
@@ -195,25 +195,25 @@ class nike_crawler_Liquidacion:
 
             #vemos todas las paginas disponibles
             self.total_pages = base_json.get('data')['products']['pages']['totalPages']
-            
+
             last_product = self.total_pages * 24
             self.array_anchors = list(range(0, last_product,24))
-    
+
         except:
             print('No se pudo acceder al url')
             self.errors.append(1)
-            
-            
+
+
     def obtain_save_data(self):
         #llamamos la funcion de llenado de informacio basica para la clase
         self.obtain_pre_info()
-        
+
         assert len(self.errors) == 0
-        assert len(self.array_anchors) > 5
+        #assert len(self.array_anchors) > 5
         for anchor in self.array_anchors:
             try:
                 base_page = 'https://api.nike.com/cic/browse/v1?queryid=products&anonymousId=DE9CEB2891D6D710B49C8CABEDC222DC&country=mx&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(MX)%26filter%3Dlanguage(es-419)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c%2C5b21a62a-0503-400c-8336-3ccfbff2a684)%26anchor%3D{}%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=es-419&localizedRangeStr=%7BlowestPrice%7D%20%E2%80%94%20%7BhighestPrice%7D'.format(anchor)
-                    
+
                 base_request = requests.get(base_page)
                 self.full_product_list = self.full_product_list + base_request.json()['data']['products']['products']
                 self.state_pages.append('OK')
@@ -221,17 +221,17 @@ class nike_crawler_Liquidacion:
                 print('No se pudo acceder al url')
                 self.errors.append(1)
                 self.state_pages.append('ERROR')
-            
+
         assert len(self.errors) == 0
         assert self.date_today != ''
-            
+
         #guardamos todos los datos
         with open('data/Liquidacion/%s.json'%self.date_today, 'w', encoding='utf-8') as f:
             json.dump(self.full_product_list, f, ensure_ascii=False, indent=4)
-            
+
         #Imprimimos el verbose
         print(self.verbose())
-        
+
     def verbose(self):
         text = '--------Seccion Liquidacion---------'+'\n'
         text+= 'STATUS: ' + self.date_today+'\n'
@@ -252,7 +252,7 @@ class nike_crawler_Liquidacion:
 class ReportTM_LIQ(ReportTM):
     def __init__(self,path):
         self.path = path
-        
+
     def generate_DF(self,name_file):
         with open(self.path+name_file, encoding='utf-8') as fh:
             data = json.load(fh)
@@ -286,11 +286,11 @@ class ReportTM_LIQ(ReportTM):
             Data_products['title'].append(product['title'])
             Data_products['date'].append(name_file[:-5])
             Data_products['url'].append(product['url'])
-        
+
         return pd.DataFrame(Data_products)
 '''
 class ReportTM_LIQ:
-    
+
     def generate_DF(self,name_file):
         with open('data/liquidacion/'+name_file, encoding='utf-8') as fh:
             data = json.load(fh)
@@ -324,7 +324,7 @@ class ReportTM_LIQ:
             Data_products['title'].append(product['title'])
             Data_products['date'].append(name_file[:-5])
             Data_products['url'].append(product['url'])
-        
+
         return pd.DataFrame(Data_products)
 
     def generate_time_serie(self,*files):
@@ -350,9 +350,9 @@ class ReportTM_LIQ:
         df_final = reduce(lambda left,right: pd.merge(left,right,on='id', how='outer'), array_dataframes)
 
         return df_final
-    
+
     def check_changes_2days(self, day):
-    
+
         #obtenemos el dia anterior a la fecha dada
         date = datetime.strptime(day, "%Y-%m-%d")
         modified_date = date - timedelta(days=1)
@@ -374,7 +374,7 @@ class ReportTM_LIQ:
     #NOTA:
     #Solo se ingresa la fecha del dia despues, el dia anterior lo se obtiene dentro de la funcion
     def check_changes_2days(self, day):
-    
+
         #obtenemos el dia anterior a la fecha dada
         date = datetime.strptime(day, "%Y-%m-%d")
         modified_date = date - timedelta(days=1)
